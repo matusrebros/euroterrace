6d7baa3e52bdb9962b65fcaaa40db89e4e389a60
matusrebros <rebros.matus@gmail.com>
2021-06-22 11:42:23 +0200

    added blog and article

---
+++ clanok.php
@@ -0,0 +1,4 @@
+<?php require_once '_partials/header.php'; ?>
+<?php require_once '_partials/form.php'; ?>
+<?php require_once '_partials/newsletter.php'; ?>
+<?php require_once '_partials/footer.php'; ?>
