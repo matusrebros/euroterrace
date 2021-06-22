cd578048cc73d75f26138416978f651cfc25ecd4
matusrebros <rebros.matus@gmail.com>
2021-06-22 16:54:50 +0200

    utf8 fixed, home image slider fixed etc

--- index.php
+++ index.php
@@ -1,219 +1,224 @@
-<?php require_once '_partials/header.php'; ?>
+<?php require_once '_partials/header.php';?>
 
-	<section class="home-hero">
-		<div class="home-hero-container">
-			<div class="home-hero-content">
-				<h1>The quick, brown<br>fox jumps over.</h1>
-				<p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced
-					by fox whelps. Bawds jog, flick quartz, vex nymphs.</p>
-				<a href="#">Button text</a>
+<section class="home-hero">
+	<div class="home-hero-container">
+		<div class="home-hero-content">
+			<h1>The quick, brown<br>fox jumps over.</h1>
+			<p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced
+				by fox whelps. Bawds jog, flick quartz, vex nymphs.</p>
+		</div>
+		<div class="home-hero-white-bars">
+			<div class="home-hero-white-bars-one"></div>
+			<div class="home-hero-white-bars-two"></div>
+			<div class="home-hero-white-bars-three"></div>
+			<a class="home-hero-main-cta" href="#">Button text</a>
+		</div>
+	</div>
+	<div class="scroll-down-element">
+		<a href="#">
+			<img src="img/scroll-down-button.svg" alt="Scroll down button">
+		</a>
+		<img class="line" src="img/line-scrolldown-element.svg" alt="Scroll down element">
+	</div>
+</section>
+
+<section class="home-introduction">
+	<div class="home-introduction-content">
+		<h2>Sed ut perspiciatis unde omnis<br>iste natus error sit.</h2>
+		<p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by
+			fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab
+			quick-jived waltz.</p>
+	</div>
+</section>
+
+<?php require_once '_partials/visualization.php';?>
+<?php require_once '_partials/filters.php';?>
+<?php require_once '_partials/about.php';?>
+
+<section class="home-counter">
+	<div class="home-counter-content">
+		<div class="home-counter-content-top">
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
 			</div>
-			<div class="home-hero-white-bars">
-				<div class="home-hero-white-bars-one"></div>
-				<div class="home-hero-white-bars-two"></div>
-				<div class="home-hero-white-bars-three"></div>
-				<a class="home-hero-main-cta" href="#">Button text</a>
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
+			</div>
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
 			</div>
 		</div>
-	</section>
-
-	<section class="home-introduction">
-		<div class="home-introduction-content">
-			<h2>Sed ut perspiciatis unde omnis<br>iste natus error sit.</h2>
-			<p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by
-				fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab
-				quick-jived waltz.</p>
-		</div>
-	</section>
-
-	<?php require_once '_partials/visualization.php'; ?>
-	<?php require_once '_partials/filters.php'; ?>
-	<?php require_once '_partials/about.php'; ?>
-
-	<section class="home-counter">
-		<div class="home-counter-content">
-			<div class="home-counter-content-top">
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
+		<div class="home-counter-content-bottom">
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
 			</div>
-			<div class="home-counter-content-bottom">
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
-				<div class="counter">
-					<strong>80</strong>
-					<span>Jedno až štvor izbových bytov.</span>
-				</div>
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
+			</div>
+			<div class="counter">
+				<strong>80</strong>
+				<span>Jedno až štvor izbových bytov.</span>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
 
-	<section class="home-gallery-slider">
-		<div class="home-gallery-slider-content">
-			<div id="splide" class="splide" data-splide='{"type":"loop"}'>
-				<div class="splide__track__wrapper">
-					<div class="splide__track">
-						<ul class="splide__list">
-							<li class="splide__slide">
-								<div class="card">
-									<div class="container">
-										<img src="./img/gallery-img.jpg" alt="Gallery Image">
-									</div>
+<section class="home-gallery-slider">
+	<div class="home-gallery-slider-content">
+		<div id="splide" class="splide" data-splide='{"type":"loop"}'>
+			<div class="splide__track_wrapper">
+				<div class="splide__track">
+					<ul class="splide__list">
+						<li class="splide__slide">
+							<div class="card">
+								<div class="container">
+									<img src="./img/gallery-img.jpg" alt="Gallery Image">
 								</div>
-							</li>
-							<li class="splide__slide">
-								<div class="card">
-									<div class="container">
-										<img src="./img/gallery-img.jpg" alt="Gallery Image">
-									</div>
+							</div>
+						</li>
+						<li class="splide__slide">
+							<div class="card">
+								<div class="container">
+									<img src="./img/gallery-img.jpg" alt="Gallery Image">
 								</div>
-							</li>
-							<li class="splide__slide">
-								<div class="card">
-									<div class="container">
-										<img src="./img/gallery-img.jpg" alt="Gallery Image">
-									</div>
+							</div>
+						</li>
+						<li class="splide__slide">
+							<div class="card">
+								<div class="container">
+									<img src="./img/gallery-img.jpg" alt="Gallery Image">
 								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="splide__arrows">
-					<button class="splide__arrow splide__arrow--prev">
-						<img class="graphs-arrow" src="./img/home-gallery-arrow-left.svg">
-					</button>
-					<button class="splide__arrow splide__arrow--next">
-						<img class="graphs-arrow rotated-arrow" src="./img/home-gallery-arrow-right.svg">
-					</button>
+							</div>
+						</li>
+					</ul>
 				</div>
 			</div>
-		</div>
-	</section>
-
-	<section class="home-standards">
-		<div class="home-standards-bars">
-			<div class="home-standards-bar-one"></div>
-			<div class="home-standards-bar-two"></div>
-		</div>
-		<div class="home-standards-content">
-			<div class="home-standards-wrapper">
-				<h3>Štandardy vybavenia EuroTerrace.</h3>
-				<div class="home-standards-container">
-					<a href="#">
-						Zistiť viac
-						<img src="./img/button-arrow.svg" alt="Button arrow">
-					</a>
-					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-						totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
-				</div>
+			<div class="splide__arrows">
+				<button class="splide__arrow splide__arrow--prev">
+					<img class="graphs-arrow" src="./img/home-gallery-arrow-left.svg">
+				</button>
+				<button class="splide__arrow splide__arrow--next">
+					<img class="graphs-arrow rotated-arrow" src="./img/home-gallery-arrow-right.svg">
+				</button>
 			</div>
 		</div>
-		<div class="home-standards-icons">
-			<fi class="home-standards-icons-container">
-				<div class="home-standards-icons-top">
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon invisible-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-				</div>
-				<div class="home-standards-icons-bottom">
-					<div class="home-standards-icon invisible-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-					<div class="home-standards-icon">
-						<div class="home-standards-icon-wrapper">
-							<img src="//" alt="Icon">
-						</div>
-						<span>Sed ut perspiciatis unde omnis iste</span>
-					</div>
-				</div>
-			</fi>
-		</div>
-	</section>
+	</div>
+</section>
 
-	<section class="home-timeline">
-		<div class="home-time-line-content">
-			<div class="timeline-content">
-				<div class="point">
-					<strong>Rezervácie</strong>
-					<span>2021</span>
-					<small>4. kvartál</small>
-					<div class="small-dot"></div>
-					<div class="big-dot"></div>
+<section class="home-standards">
+	<div class="home-standards-bars">
+		<div class="home-standards-bar-one"></div>
+		<div class="home-standards-bar-two"></div>
+	</div>
+	<div class="home-standards-content">
+		<div class="home-standards-wrapper">
+			<h3>Štandardy vybavenia EuroTerrace.</h3>
+			<div class="home-standards-container">
+				<a href="#">
+					Zistiť viac
+					<img src="./img/button-arrow.svg" alt="Button arrow">
+				</a>
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+					totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+			</div>
+		</div>
+	</div>
+	<div class="home-standards-icons">
+		<fi class="home-standards-icons-container">
+			<div class="home-standards-icons-top">
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
 				</div>
-				<div class="point">
-					<strong>Výstavba</strong>
-					<span>2022</span>
-					<small>1. kvartál</small>
-					<div class="small-dot"></div>
-					<div class="big-dot"></div>
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
 				</div>
-				<div class="point">
-					<strong>Kolaudácia</strong>
-					<span>2022</span>
-					<small>3. kvartál</small>
-					<div class="small-dot"></div>
-					<div class="big-dot"></div>
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
+				</div>
+				<div class="home-standards-icon invisible-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
 				</div>
 			</div>
-			<div class="timeline-line"></div>
+			<div class="home-standards-icons-bottom">
+				<div class="home-standards-icon invisible-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
+				</div>
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
+				</div>
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
+				</div>
+				<div class="home-standards-icon">
+					<div class="home-standards-icon-wrapper">
+						<img src="//" alt="Icon">
+					</div>
+					<span>Sed ut perspiciatis unde omnis iste</span>
+				</div>
+			</div>
+		</fi>
+	</div>
+</section>
+
+<section class="home-timeline">
+	<div class="home-time-line-content">
+		<div class="timeline-content">
+			<div class="point">
+				<strong>Rezervácie</strong>
+				<span>2021</span>
+				<small>4. kvartál</small>
+				<div class="small-dot"></div>
+				<div class="big-dot"></div>
+			</div>
+			<div class="point">
+				<strong>Výstavba</strong>
+				<span>2022</span>
+				<small>1. kvartál</small>
+				<div class="small-dot"></div>
+				<div class="big-dot"></div>
+			</div>
+			<div class="point">
+				<strong>Kolaudácia</strong>
+				<span>2022</span>
+				<small>3. kvartál</small>
+				<div class="small-dot"></div>
+				<div class="big-dot"></div>
+			</div>
 		</div>
-	</section>
+		<div class="timeline-line"></div>
+	</div>
+</section>
 
 
-	<?php require_once '_partials/novinky.php'; ?>
-	<?php require_once '_partials/developer.php'; ?>
-	<?php require_once '_partials/partners.php'; ?>
-	<?php require_once '_partials/form.php'; ?>
-	<?php require_once '_partials/newsletter.php'; ?>
-	<?php require_once '_partials/footer.php'; ?>
+<?php require_once '_partials/novinky.php';?>
+<?php require_once '_partials/developer.php';?>
+<?php require_once '_partials/partners.php';?>
+<?php require_once '_partials/form.php';?>
+<?php require_once '_partials/newsletter.php';?>
+<?php require_once '_partials/footer.php';?>
